1c11d425265c1ccd1508331c7a232f1ac5af37c9
Igor Trofimov <igortr333@gmail.com>
2025-12-12 12:18:56 +0200

    update image 12 dec

--- src/commands/advent.rs
+++ src/commands/advent.rs
@@ -12,7 +12,7 @@ use crate::db;
 use super::common::{escape_markdown_v2, Contents};
 
 const PHOTO_URL: &str =
-    "https://raw.githubusercontent.com/Traf333/ecobot/refs/heads/main/src/images/advent8.jpg";
+    "https://raw.githubusercontent.com/Traf333/ecobot/refs/heads/main/src/images/advent9.jpg";
 
 pub struct AdventCommand;
 
